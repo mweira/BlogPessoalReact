deb6dcf1513ef7f6a11ce766f57d7238867b685f
João Pedro <jpmeira020@gmail.com>
2022-06-01 20:36:20 -0300

    Commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
-import {  Link, Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import Home from './paginas/home/Home'
-import Login from './paginas/login/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
 import './App.css';
-
+import Login from './paginas/login/Login';
 
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div style={{ minHeight: '100vh' }}>
-        <Routes> 
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </div>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
     </Router>
-  )
+
+  );
 }
 
 export default App;
